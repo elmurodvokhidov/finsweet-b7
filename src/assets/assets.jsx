019c69d8b3892aa44f862506fd5ea0fe56284616
islom <islomxonahmedov7@gmail.com>
2023-12-12 20:52:38 +0500

    navbar va footer tugadi

--- src/assets/assets.jsx
+++ src/assets/assets.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export function GlobalBtn({ specialClass, text }) {
     return (
@@ -11,3 +11,14 @@ export function GlobalLink({ specialClass, text, to }) {
         <Link className={specialClass} to={to}>{text}</Link>
     )
 };
+export function Link2({na,nam,nam1,nam2,nam3}) {
+    return(
+        <div className="box2">
+            <h2>{na}</h2>
+            <NavLink className="i"  to={"about"}>{nam}</NavLink>
+            <NavLink className="i" to={"careers"}> {nam1}</NavLink>
+            <NavLink className="i" to={"services"}>{nam2}</NavLink>
+            <NavLink className="i" to={"blog"}>{nam3}</NavLink>
+        </div>
+    )
+}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,52 @@
 import { Link } from "react-router-dom"
+import { FaFacebookF } from "react-icons/fa";
+import { CiYoutube } from "react-icons/ci";
+import { FaInstagram } from "react-icons/fa";
+import { CiTwitter } from "react-icons/ci";
+import logo1 from "../../public/img/Logo 2.svg"
+import right from "../../public/img/right.png"
+import left from "../../public/img/left.png"
+import { Link2 } from "../assets/assets";
 function Footer() {
     return (
         <div className="footer">
-            <div className="buttonleft">
-                <a href="#">© All rights reserved – Finsweet</a>
+            <div className="top">
+                <div className="box1">
+                    <a https="/"><img src={logo1} alt="logoo" /></a>
+                    <h1>Bespoke software <br /> solutions</h1>
+                    <div className="icon">
+                        <FaFacebookF />
+                        <CiYoutube />
+                        <FaInstagram />
+                        <CiTwitter />
+                    </div>
+                </div>
+                <div className="box2">
+                    <Link2 na={"Company"} nam={"About Us"} nam1={"Careers"} nam2={"Services"} nam3={"Blog"} />
+                </div>
+                <div className="box3">
+                    <Link2 na={"Connect"} nam={"hi@finsweet.com"} nam1={"+(123) 456-7890"} />
+                </div>
+                <div className="box4">
+                    <h2>Join Newsletter</h2>
+                    <input className="inputone" type="email" placeholder="Enter your email" />
+                    <button>Submit</button>
+                </div>
             </div>
-            <div className="buttonright">
-                <Link to={'privacy'}>Privacy Policy</Link>
-                <Link to={'terms'}>Terms & Conditions</Link>
+            <div className="buttom">
+                <div className="buttonleft">
+                    <a href="#">© All rights reserved – Finsweet</a>
+                </div>
+                <div className="buttonright">
+                    <Link className="i" to={'privacy'}>Privacy Policy</Link>
+                    <Link className="i" to={'terms'}>Terms & Conditions</Link>
+                </div>
             </div>
+            <img className="right" src={right} alt="rasim" />
+
         </div>
     )
 }
+
 
 export default Footer
