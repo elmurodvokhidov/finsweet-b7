c54a6335a973854c91dea2520227fad715308472
Hojiakbar Name <bekmirzayevhojiakbar306@example.com>
2023-12-13 11:27:07 +0500

    section 2 pashti tayyor

--- src/assets/assets.jsx
+++ src/assets/assets.jsx
@@ -12,14 +12,36 @@ export function GlobalLink({ specialClass, text, to }) {
     )
 };
 
-export function Link2({na,nam,nam1,nam2,nam3}) {
-    return(
+export function Link2({ na, nam, nam1, nam2, nam3 }) {
+    return (
         <div className="box2">
             <h2>{na}</h2>
-            <NavLink className="i"  to={"about"}>{nam}</NavLink>
+            <NavLink className="i" to={"about"}>{nam}</NavLink>
             <NavLink className="i" to={"careers"}> {nam1}</NavLink>
             <NavLink className="i" to={"services"}>{nam2}</NavLink>
             <NavLink className="i" to={"blog"}>{nam3}</NavLink>
         </div>
     )
 }
+export function Cardone() {
+    return (
+        <div className="asil">
+            <h1 className="xat1">We help more than 1500 <br /> companies from all sectors</h1>
+            <h4 className="xat2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros <br /> blandit, hendrerit elit et, mattis purus. Vivamus commodo suscipit tellus et <br /> pellentesque.</h4>
+
+        </div>
+    )
+}
+
+export function Cardtwo({rasm1 , matn1 , matn2 ,matn3 , rasm2}) {
+    return(
+        <div className="qolib">
+            <img src={rasm1} alt="hoji"  className="rasm"/>
+            <h2 className="m1">{matn1}</h2>
+            <h4 className="k1">{matn2}</h4>
+            <h3 className="h3">{matn3}</h3>
+            <img src={rasm2} alt="as" className="icon999"/>
+        </div>
+    )
+}
+
