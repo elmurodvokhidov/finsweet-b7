1e5e1ddadbee7d731532c5549b3718547d496810
Hojiakbar Name <bekmirzayevhojiakbar306@example.com>
2023-12-13 14:22:48 +0500

    sxc

--- src/assets/assets.jsx
+++ src/assets/assets.jsx
@@ -105,22 +105,4 @@ export function Cardfour({say , say2 ,say3 }){
     )
 }
 
-export function  Cardthere({wer ,wer1}){
-    return(
-        <div className="ota">
-            <h1 className="wer">{wer}</h1>
-            <h4 className="wer1">{wer1}</h4>
-        </div>
-    )
-}
-
-export function Cardfour({say , say2 ,say3 }){
-    return(
-        <div className="ona">
-            <img src={say} alt="asa" className="qiz" />
-            <h2 className="xat8">{say2}</h2>
-            <h4 className="xat9">{say3}</h4>
-        </div>
-    )
-}
 
